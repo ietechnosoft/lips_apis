fb3923562da811d5ab92f23760ae156c98871bc1
Nikhil Sharma <nikhil.rsharma@hotmail.com>
2021-02-09 19:33:21 +0530

    commit

--- login.php
+++ login.php
@@ -25,19 +25,24 @@
     $chk1=mysqli_query($con,"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$dbname'");
     //'luckycollegeerp2020_21'
     
-    if($chk=mysqli_fetch_row($chk1)) {
+    if($chk=mysqli_fetch_row($chk1)) 
+    {
         // echo "select s_id,name,f_id,sem,sem2,pic from $dbname.$_REQUEST[college] where uname='" . $username . "' and password='" . $password . "' and status=1";
         $l1=mysqli_query($con,"select s_id,name,f_id,sem,sem2,pic from $dbname.$_REQUEST[college] where uname='".$username."' and password='".$password."' and status=1");
-        if($l=mysqli_fetch_row($l1)) {
+        if($l=mysqli_fetch_row($l1)) 
+        {
             $str = explode("_", $_SESSION['cursession']);
             $str[0]++;
             $str[1]++;
             $_REQUEST['cursession1'] = $str[0]."_".$str[1];
             $dbname = "luckycollegeerp".$_REQUEST['cursession1'];
+            
             $chk1=mysqli_query($con,"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$dbname'");
-            if($chk=mysqli_fetch_row($chk1)) {
+            if($chk=mysqli_fetch_row($chk1)) 
+            {
                 $l2=mysqli_query($con,"select s_id,name,f_id,sem,sem2,pic from $dbname.$_REQUEST[college] where uname='" . $username . "' and password='" . $password . "' and status=1 ");
-                if($l3=mysqli_fetch_row($l2)) {
+                if($l3=mysqli_fetch_row($l2)) 
+                {
                     $login['college_session'] = str_replace("_","-", $_SESSION['cursession1']);
                     $login['user_id']=$l3[0];
                     $login['name'] = $l3[1];
@@ -46,13 +51,16 @@
                     $login['sem1'] = $l3[4];
                     $login['pic'] = $l3[5];
                     $login['user_type'] = "1";
+                    $login['college'] = "1";
 
                     $f = mysqli_fetch_row(mysqli_query($con, "select batchyear,course,yearwise from $fees where f_id='$l3[2]'"));
                     $login['batch'] = $f[0];
                     $login['course'] = $f[1];
                     $login['year'] = $f[2];
 
-                } else {
+                } 
+                else 
+                {
                     $login['college_session'] = str_replace("_", "-", $_SESSION['cursession']);
                     $login['user_id'] = $l[0];
                     $login['name'] = $l[1];
@@ -61,13 +69,16 @@
                     $login['sem2'] = $l[4];
                     $login['pic'] = $l[5];
                     $login['user_type'] = "1";
+                    $login['college'] = "1";
 
                     $f = mysqli_fetch_row(mysqli_query($con, "select batchyear,course,yearwise from $fees where f_id='$l[2]'"));
                     $login['batch'] = $f[0];
                     $login['course'] = $f[1];
                     $login['year'] = $f[2];
                 }
-            } else {
+            } 
+            else 
+            {
                 $login['college_session'] = str_replace("_", "-", $_SESSION['cursession']);
                 $login['user_id'] = $l[0];
                 $login['name'] = $l[1];
@@ -76,26 +87,29 @@
                 $login['sem2'] = $l[4];
                 $login['pic'] = $l[5];
                 $login['user_type'] = "1";
+                $login['college'] = "1";
 
                 $f = mysqli_fetch_row(mysqli_query($con, "select batchyear,course,yearwise from $fees where f_id='$l[2]'"));
-                    $login['batch'] = $f[0];
-                    $login['course'] = $f[1];
-                    $login['year'] = $f[2];
+                $login['batch'] = $f[0];
+                $login['course'] = $f[1];
+                $login['year'] = $f[2];
             }
-        } else {
+        } 
+        else 
+        {
             $str=explode("_", $_SESSION['cursession']);
             $str[0]++;
             $str[1]++;
             $_SESSION['cursession']=$str[0]."_".$str[1];
             $dbname="luckycollegeerp". $_SESSION['cursession'];
+
             // echo "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$dbname'";
             $chk1=mysqli_query($con,"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$dbname'");		
-            
-            if($chk=mysqli_fetch_row($chk1)) {
-                $l1=mysqli_query($con,"select s_id,name,f_id,sem,sem2,pic from $dbname.$_REQUEST[college] where  uname='" . $username . "' and password='" . $password . "'  and status=1");
-                
-                if($l=mysqli_fetch_row($l1)) {
-
+            if($chk=mysqli_fetch_row($chk1)) 
+            {
+                $l1=mysqli_query($con,"select s_id,name,f_id,sem,sem2,pic from $dbname.$_REQUEST[college] where  uname='" . $username . "' and password='" . $password . "'  and status=1");    
+                if($l=mysqli_fetch_row($l1)) 
+                {
                     $login['college_session'] = str_replace("_", "-", $_REQUEST['cursession']);
                     $login['user_id'] = $l[0];
                     $login['name'] = $l[1];
@@ -104,22 +118,138 @@
                     $login['sem2'] = $l[4];
                     $login['pic'] = $l[5];
                     $login['user_type'] = "1";
+                    $login['college'] = "1";
 
                     $f = mysqli_fetch_row(mysqli_query($con, "select batchyear,course,yearwise from $fees where f_id='$l[2]'"));
                     $login['batch'] = $f[0];
                     $login['course'] = $f[1];
                     $login['year'] = $f[2];
-                } else {
+                } 
+                else 
+                {
                     $RESPONSE['result'] = "fail";
                     $RESPONSE['message'] = "Invalid Username or Password!!!";
 
                     $login = null;
                 }
             } else {
-                $RESPONSE['result'] = "fail";
-                $RESPONSE['message'] = "Invalid Username or Password!!!";
+                $gdcol = "gdcol2";
+                $_REQUEST['college'] = "gdcol2";
+                $fees = "fee_structure2";
+                $_SESSION['cursession'] =    $Y . "_" . (substr($Y, 2) + 1);
+                $dbname = "luckycollegeerp" . $_SESSION['cursession'];
+                
+                // echo "select s_id,name,f_id,sem,sem2,pic from $dbname.$_REQUEST[college] where uname='" . $username . "' and password='" . $password . "' and status=1";
+                $l1 = mysqli_query($con, "select s_id,name,f_id,sem,sem2,pic from $dbname.$_REQUEST[college] where uname='" . $username . "' and password='" . $password . "' and status=1");
+                if ($l = mysqli_fetch_row($l1)) 
+                {
+                    $str = explode("_", $_SESSION['cursession']);
+                    $str[0]++;
+                    $str[1]++;
+                    $_REQUEST['cursession1'] = $str[0] . "_" . $str[1];
+                    $dbname = "luckycollegeerp" . $_REQUEST['cursession1'];
+                    
+                    $chk1 = mysqli_query($con, "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$dbname'");
+                    if ($chk = mysqli_fetch_row($chk1)) 
+                    {
+                        $l2 = mysqli_query($con, "select s_id,name,f_id,sem,sem2,pic from $dbname.$_REQUEST[college] where uname='" . $username . "' and password='" . $password . "' and status=1 ");
+                        if ($l3 = mysqli_fetch_row($l2)) 
+                        {
+                            $login['college_session'] = str_replace("_", "-", $_SESSION['cursession1']);
+                            $login['user_id'] = $l3[0];
+                            $login['name'] = $l3[1];
+                            $login['f_id'] = $l3[2];
+                            $login['sem'] = $l3[3];
+                            $login['sem1'] = $l3[4];
+                            $login['pic'] = $l3[5];
+                            $login['user_type'] = "1";
+                            $login['college'] = "2";
 
-                $login = null;
+                            $f = mysqli_fetch_row(mysqli_query($con, "select batchyear,course,yearwise from $fees where f_id='$l3[2]'"));
+                            $login['batch'] = $f[0];
+                            $login['course'] = $f[1];
+                            $login['year'] = $f[2];
+                        } 
+                        else 
+                        {
+                            $login['college_session'] = str_replace("_", "-", $_SESSION['cursession']);
+                            $login['user_id'] = $l[0];
+                            $login['name'] = $l[1];
+                            $login['f_id'] = $l[2];
+                            $login['sem1'] = $l[3];
+                            $login['sem2'] = $l[4];
+                            $login['pic'] = $l[5];
+                            $login['user_type'] = "1";
+                            $login['college'] = "2";
+
+                            $f = mysqli_fetch_row(mysqli_query($con, "select batchyear,course,yearwise from $fees where f_id='$l[2]'"));
+                            $login['batch'] = $f[0];
+                            $login['course'] = $f[1];
+                            $login['year'] = $f[2];
+                        }
+                    } 
+                    else 
+                    {
+                        $login['college_session'] = str_replace("_", "-", $_SESSION['cursession']);
+                        $login['user_id'] = $l[0];
+                        $login['name'] = $l[1];
+                        $login['f_id'] = $l[2];
+                        $login['sem1'] = $l[3];
+                        $login['sem2'] = $l[4];
+                        $login['pic'] = $l[5];
+                        $login['user_type'] = "1";
+                        $login['college'] = "2";
+
+                        $f = mysqli_fetch_row(mysqli_query($con, "select batchyear,course,yearwise from $fees where f_id='$l[2]'"));
+                        $login['batch'] = $f[0];
+                        $login['course'] = $f[1];
+                        $login['year'] = $f[2];
+                    }
+                } 
+                else 
+                {
+                    $str = explode("_", $_SESSION['cursession']);
+                    $str[0]++;
+                    $str[1]++;
+                    $_SESSION['cursession'] = $str[0] . "_" . $str[1];
+                    $dbname = "luckycollegeerp" . $_SESSION['cursession'];
+
+                    // echo "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$dbname'";
+                    $chk1 = mysqli_query($con, "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$dbname'");
+                    if ($chk = mysqli_fetch_row($chk1)) 
+                    {
+                        $l1 = mysqli_query($con, "select s_id,name,f_id,sem,sem2,pic from $dbname.$_REQUEST[college] where  uname='" . $username . "' and password='" . $password . "'  and status=1");
+                        if ($l = mysqli_fetch_row($l1)) 
+                        {
+                            $login['college_session'] = str_replace("_", "-", $_REQUEST['cursession']);
+                            $login['user_id'] = $l[0];
+                            $login['name'] = $l[1];
+                            $login['f_id'] = $l[2];
+                            $login['sem1'] = $l[3];
+                            $login['sem2'] = $l[4];
+                            $login['pic'] = $l[5];
+                            $login['user_type'] = "1";
+                            $login['college'] = "2";
+
+                            $f = mysqli_fetch_row(mysqli_query($con, "select batchyear,course,yearwise from $fees where f_id='$l[2]'"));
+                            $login['batch'] = $f[0];
+                            $login['course'] = $f[1];
+                            $login['year'] = $f[2];
+                        } 
+                        else 
+                        {
+                            $RESPONSE['result'] = "fail";
+                            $RESPONSE['message'] = "Invalid Username or Password!!!";
+                            $login = null;
+                        }
+                    } 
+                    else 
+                    {
+                        $RESPONSE['result'] = "fail";
+                        $RESPONSE['message'] = "Invalid Username or Password!!!";
+                        $login = null;
+                    }
+                }
             }
         }
     } else {
