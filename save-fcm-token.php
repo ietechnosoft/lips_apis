3849dd03c5b2b7e7b6beaba6db4eaf1f9a816d24
Nikhil Sharma <nikhil.rsharma@hotmail.com>
2021-02-15 14:56:37 +0530

    commit

--- save-fcm-token.php
+++ save-fcm-token.php
@@ -7,11 +7,11 @@
     $RESPONSE = array();
     $RESPONSE['result'] = "success";
 
-    $d1=mysqli_query($con,"select * from app_user_auth where user_id='$_REQUEST[s_id]' and user_type='$_REQUEST[user_type]' and college_id='$_REQUEST[college_id]' ");
+    $d1=mysqli_query($con1,"select * from app_user_auth where user_id='$_REQUEST[s_id]' and user_type='$_REQUEST[user_type]' and college_id='$_REQUEST[college_id]' ");
     if(mysqli_num_rows($d1) == 0) {
-        mysqli_query($con, "insert into app_user_auth set user_id='$_REQUEST[s_id]', user_type='$_REQUEST[user_type]', college_id='$_REQUEST[college_id]', device_id='$_REQUEST[device_id]', fcm_token='$_REQUEST[fcm_token]' ");
+        mysqli_query($con1, "insert into app_user_auth set user_id='$_REQUEST[s_id]', user_type='$_REQUEST[user_type]', college_id='$_REQUEST[college_id]', device_id='$_REQUEST[device_id]', fcm_token='$_REQUEST[fcm_token]' ");
     } else {
-        mysqli_query($con, "update app_user_auth set fcm_token='$_REQUEST[fcm_token]' where user_id='$_REQUEST[s_id]' and user_type='$_REQUEST[user_type]' and college_id='$_REQUEST[college_id]' and device_id='$_REQUEST[device_id]' ");
+        mysqli_query($con1, "update app_user_auth set fcm_token='$_REQUEST[fcm_token]' where user_id='$_REQUEST[s_id]' and user_type='$_REQUEST[user_type]' and college_id='$_REQUEST[college_id]' and device_id='$_REQUEST[device_id]' ");
     }
 
     echo json_encode($RESPONSE);
