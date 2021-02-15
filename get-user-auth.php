e5065772c1c5b72a427456fb74058411d5989cb6
Nikhil Sharma <nikhil.rsharma@hotmail.com>
2021-02-15 15:27:49 +0530

    commit

--- get-user-auth.php
+++ get-user-auth.php
@@ -5,12 +5,20 @@
 
 
     $RESPONSE = array();
-    $RESPONSE['result'] = "success";
 
     $d1=mysqli_query($con,"select device_id from app_user_auth where user_id='$_REQUEST[s_id]' and user_type='$_REQUEST[user_type] and college_id='$_REQUEST[college_id]' ");
     
-    $d=mysqli_fetch_row($d1);
-    $RESPONSE['device_id'] = $d[0];
+    if(mysqli_num_rows($d1) == 0) {
+        $RESPONSE['result'] = "success";
+    } else {
+        $d=mysqli_fetch_row($d1);
+        if($d[0] == $_REQUEST['device_id']) {
+            $RESPONSE['result'] = "success";
+        } else {
+            $RESPONSE['result'] = "fail";
+            $RESPONSE['message'] = "It's look like you have logged in on another device..";
+        }
+    }
 
     echo json_encode($RESPONSE);
 ?>
